b9ae6e90a570f6e7e6b93ab556bb7ef7a033d508
Edward Phillips <edd0ddi0@gmail.com>
2020-05-26 16:42:08 +0100

    Server must respond to /api/

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+app.get('/api/', cors(), async(req, res, next) => {
+  try {
+    const message = "Hello World!";
+    res.json({ message });
+  } catch (err) {
+    next(err);
+  }
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Listening on ${PORT}`)
+})
