6535dc423be88c0d8d8cdeb703408abde85f36d7
Edward Phillips <edd0ddi0@gmail.com>
2020-05-27 16:22:22 +0100

    broken commit: socket connection not found

--- client/src/App.js
+++ client/src/App.js
@@ -1,34 +1,47 @@
 import React, { Component } from 'react';
-import './App.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Join from './component/Join.js';
+import Chat from './component/Chat.js';
 
-import User from "./component/user.js"
-
-class App extends Component {
-
-  state = {
-    message: ''
-  }
-
-
-  componentDidMount() {
-    this.fetchMessage()
-  }
-
-  fetchMessage = async () => {
-    const response = await fetch('/api/')
-    const resultObject = await response.json()
-    const message = resultObject.message
-    this.setState({ message })
-  }
-
-  render() {
-    return (
-      <div> 
-        <p>{this.state.message}</p>
-        <User />
-      </div>
-      
-    )
-  }
+const App = () => {
+  return(
+    <Router>
+      <Route path='/' exact component={ Join }/>
+      <Route path='/chat' exact component={ Chat }/>
+    </Router>
+    );
 }
+
 export default App;
+// import './App.css';
+
+// import User from "./component/User.js"
+
+// class App extends Component {
+
+//   state = {
+//     message: ''
+//   }
+
+
+//   componentDidMount() {
+//     this.fetchMessage()
+//   }
+
+//   fetchMessage = async () => {
+//     const response = await fetch('/api/')
+//     const resultObject = await response.json()
+//     const message = resultObject.message
+//     this.setState({ message })
+//   }
+
+//   render() {
+//     return (
+//       <div> 
+//         <p>{this.state.message}</p>
+//         <User />
+//       </div>
+      
+//     )
+//   }
+// }
