8a4b80cc9599fbd4aea51d38ecfff1eae13e7551
Phil Vigus <philipvigus@yahoo.co.uk>
2020-05-28 16:46:34 +0100

    Fix: Message map function key missing

--- client/src/component/Chat.js
+++ client/src/component/Chat.js
@@ -1,13 +1,12 @@
 import React, { useState, useEffect } from "react";
 import queryString from "query-string";
 import io from "socket.io-client";
-import { Link } from "react-router-dom";
 
-const Chat = ({ location }) => { 
-  const [name, setName] = useState('')
-  const [message, setMessage] = useState('')
-  const [messages, setMessages] = useState([])
-  const ENDPOINT = 'http://localhost:5000'
+const Chat = ({ location }) => {
+  const [name, setName] = useState("");
+  const [message, setMessage] = useState("");
+  const [messages, setMessages] = useState([]);
+  const ENDPOINT = "http://localhost:5000";
 
   useEffect(() => {
     const { name } = queryString.parse(location.search);
@@ -19,23 +18,37 @@ const Chat = ({ location }) => {
 
   const handleSubmitMessage = (e) => {
     e.preventDefault();
-    
-    setMessages([...messages, `${name}: ${message}`])
 
-    setMessage('')
+    setMessages([...messages, `${name}: ${message}`]);
+
+    setMessage("");
   };
-  
-  return(
+
+  return (
     <div>
       <h1>Chat</h1>
       <div className="message-box">
         <div>Welcome {name} to the room</div>
-        <div className="display-message-container">{messages.map((mes) => {return(<div className="display-message">{mes}</div>)})}</div>
+        <div className="display-message-container">
+          {messages.map((mes, index) => {
+            return (
+              <div key={index} className="display-message">
+                {mes}
+              </div>
+            );
+          })}
+        </div>
       </div>
-      <input type="text" className="chat-input" placeholder="Enter message here" value={message} onChange={(e) => setMessage(e.target.value)} />
+      <input
+        type="text"
+        className="chat-input"
+        placeholder="Enter message here"
+        value={message}
+        onChange={(e) => setMessage(e.target.value)}
+      />
       <button onClick={handleSubmitMessage}>Submit</button>
     </div>
   );
-}
+};
 
 export default Chat;
