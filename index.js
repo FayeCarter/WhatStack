56973eb82bf9e92efd553ea8543fdb6a5541d8ba
FayeCarter <fayecarter333@gmail.com>
2020-05-29 11:44:47 +0100

    Feat: add login route with github api

--- index.js
+++ index.js
@@ -7,9 +7,24 @@ const app = express();
 app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
 const server = http.createServer(app);
 const io = socketio(server);
+require("dotenv").config()
+
+const client_id=process.env.GITHUB_CLIENT_ID
+const client_secret=process.env.GITHUB_CLIENT_SECRET
 
 app.get("/", cors(), (req, res) => {
   res.send("Server is up and running");
+});
+
+app.get("/login", cors(), (req, res) => {
+  const redirect_uri = "http://localhost:5000/login/callback";
+  res.redirect(
+    `https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}&redirect_uri=${redirect_uri}`
+  );
+});
+
+app.get("/login/callback", cors(), (req, res) => {
+  res.send("next");
 });
 
 app.use(cors());
