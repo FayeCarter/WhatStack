44a6b6cdc371cf34317e70a8c48e4994c3a5760e
Steven-Klavins <stevenklavins94@gmail.com>
2020-05-29 15:15:18 +0100

    Refactor: Return user object JSON

--- router.js
+++ router.js
@@ -5,8 +5,8 @@ const cookieSession = require('cookie-session')
 const cors = require('cors');
 require("dotenv").config()
 
-const client_id=process.env.GITHUB_CLIENT_ID
-const client_secret=process.env.GITHUB_CLIENT_SECRET
+const client_id = process.env.GITHUB_CLIENT_ID
+const client_secret = process.env.GITHUB_CLIENT_SECRET
 const cookie_secret = process.env.COOKIE_SECRET
 
 router.use(
@@ -26,7 +26,11 @@ router.get("/login", cors(), (req, res) => {
   );
 });
 
-async function getAccessToken({ code, client_id, client_secret }) {
+async function getAccessToken({
+  code,
+  client_id,
+  client_secret
+}) {
   const request = await fetch("https://github.com/login/oauth/access_token", {
     method: "POST",
     headers: {
@@ -54,11 +58,16 @@ async function fetchGitHubUser(token) {
 
 router.get("/login/callback", cors(), async (req, res) => {
   const code = req.query.code;
-  const access_token = await getAccessToken({ code, client_id, client_secret });
+  const access_token = await getAccessToken({
+    code,
+    client_id,
+    client_secret
+  });
   const user = await fetchGitHubUser(access_token);
   if (user) {
     req.session.access_token = access_token;
     req.session.githubname = user.login;
+    req.session.githubID = user.id;
     res.redirect("/user");
   } else {
     res.send("Login did not succeed!");
@@ -66,8 +75,13 @@ router.get("/login/callback", cors(), async (req, res) => {
 });
 
 router.get('/user', cors(), (req, res) => {
-  if(req.session.githubname) {
-    res.json({username: req.session.githubname})
+  if (req.session.githubname) {
+    res.json({
+      user: {
+        id: req.session.githubID,
+        username: req.session.githubname
+      }
+    })
   } else {
     res.send('Not coming in')
   }
