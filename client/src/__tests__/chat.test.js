59a9a9ac8f18d17c1624d4f498b6bc162e04fd3f
FayeCarter <fayecarter333@gmail.com>
2020-05-30 16:50:02 +0100

    Test: test rendering of rooms component

--- client/src/__tests__/chat.test.js
+++ client/src/__tests__/chat.test.js
@@ -10,15 +10,18 @@ describe("Chat.", () => {
         <Chat username='ED' room='javascript'/>
       </Router>
     );
+    
     expect(screen.getByRole("button", { name: "Submit" })).toBeInTheDocument();
     expect(screen.getByPlaceholderText("Enter message here")).toBeInTheDocument();
   });
+
   test("renders user's name and room name", () => {
     render(
       <Router>
         <Chat username='ED' room='javascript'/>
       </Router>
     );
+
     expect(screen.getByRole("heading", { name: "javascript Chat" })).toBeInTheDocument();
     expect(screen.getByText('Welcome ED to the room')).toBeInTheDocument();
   })
