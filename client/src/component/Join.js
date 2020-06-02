7c632f0df6f6b884956c340982faa7361665f75d
Phil Vigus <philipvigus@yahoo.co.uk>
2020-06-02 17:38:16 +0100

    Fix: correct error in join redirect

--- client/src/component/Join.js
+++ client/src/component/Join.js
@@ -1,12 +1,14 @@
 import React from "react";
+//require("dotenv").config();
 
 const Join = () => {
+  console.log(process.env);
   return (
     <div>
       <h1>Join</h1>
       <button
         onClick={() => {
-          window.location.href = "http://whatstack.herokuapp.com/login";
+          window.location.href = `${process.env.REACT_APP_BACKEND}/login`;
         }}
       >
         Github login
