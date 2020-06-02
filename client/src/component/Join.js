5769bc07843c795579e299f9ffa5930f5dc90520
Steven <Steven@Stevens-iMac.local>
2020-06-02 20:20:34 +0100

    Styling: basic css applied to landing page

--- client/src/component/Join.js
+++ client/src/component/Join.js
@@ -1,20 +1,27 @@
-import React from "react";
+import React from 'react'
 //require("dotenv").config();
 
 const Join = () => {
-  console.log(process.env);
+  console.log(process.env)
   return (
-    <div>
-      <h1>Join</h1>
-      <button
-        onClick={() => {
-          window.location.href = `${process.env.REACT_APP_BACKEND}/login`;
-        }}
-      >
-        Github login
-      </button>
-    </div>
-  );
-};
+    <div className="body">
+      <div className="content">
+        <h1>
+          <span className="thin">What</span>
+          <span className="thick">stack</span>
+        </h1>
+        <h2>Login with Github to join now!</h2>
 
-export default Join;
+        <button
+          onClick={() => {
+            window.location.href = `${process.env.REACT_APP_BACKEND}/login`
+          }}
+        >
+          Github login
+        </button>
+      </div>
+    </div>
+  )
+}
+
+export default Join
