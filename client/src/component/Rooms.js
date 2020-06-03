2f41b32a66405c048f49a9cf074b965717d419b5
Steven-Klavins <stevenklavins94@gmail.com>
2020-06-03 20:28:54 +0100

    Styling: rooms page have CSS

--- client/src/component/Rooms.js
+++ client/src/component/Rooms.js
@@ -1,68 +1,70 @@
-import React, { useState, useEffect } from "react";
-import io from "socket.io-client";
-import { Link } from "react-router-dom";
-import cookie from "react-cookies";
-let socket;
+import React, { useState, useEffect } from 'react'
+import io from 'socket.io-client'
+import { Link } from 'react-router-dom'
+import cookie from 'react-cookies'
+let socket
 
 const Rooms = ({ setRoom, username, setUsername }) => {
-  const [userRoom, setUserRoom] = useState("");
-  const [roomList, setRoomList] = useState([]);
-  const ENDPOINT = process.env.ENDPOINT;
+  const [userRoom, setUserRoom] = useState('')
+  const [roomList, setRoomList] = useState([])
+  const ENDPOINT = process.env.ENDPOINT
 
   useEffect(() => {
-    socket = io(ENDPOINT);
-    socket.emit("requestRoomList");
-    setUsername(cookie.load("githubname"));
-  }, []);
+    socket = io(ENDPOINT)
+    socket.emit('requestRoomList')
+    setUsername(cookie.load('githubname'))
+  }, [])
 
   useEffect(() => {
-    socket.on("roomList", ({ roomList }) => {
-      setRoomList(roomList);
-    });
-  });
+    socket.on('roomList', ({ roomList }) => {
+      setRoomList(roomList)
+    })
+  })
 
   return (
-    <div>
-      <h1>Whatstack</h1>
+    <div className="roomContainer">
       <div className="topic-box">
-        <div>Welcome to Whatstack {username}</div>
+        <h2>Hi, {username}!</h2>
+        <h2>Join a chat or create one</h2>
       </div>
-      <input
-        type="text"
-        onChange={(event) => {
-          setUserRoom(event.target.value);
-        }}
-        className="chat-input"
-        placeholder="What is on your mind?"
-      />
-      <Link
-        to="/chat"
-        onClick={() => {
-          setRoom(userRoom);
-        }}
-      >
-        <button>Submit</button>
-      </Link>
+      <div className="roomInput">
+        <input
+          type="text"
+          onChange={(event) => {
+            setUserRoom(event.target.value)
+          }}
+          className="chat-input"
+          placeholder="What do you want to chat?"
+        />
+        <Link
+          to="/chat"
+          onClick={() => {
+            setRoom(userRoom)
+          }}
+        >
+          <button className="roomButton">Join</button>
+        </Link>
+      </div>
       <div className="room-list">
         <div>
           {roomList.map((room, index) => {
             return (
-              <div key={index}>
+              <div className="rectangle" key={index}>
                 <Link
                   to="/chat"
                   onClick={() => {
-                    setRoom(room);
+                    setRoom(room)
                   }}
                 >
                   {room}
                 </Link>
               </div>
-            );
+            )
           })}
         </div>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Rooms;
+export default Rooms
