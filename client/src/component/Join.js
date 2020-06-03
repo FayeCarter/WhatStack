036ceaaa50db6264681e28203ea8eaf133db26cc
Steven <Steven@Stevens-iMac.local>
2020-06-03 16:38:50 +0100

    Styling: landing page styling added

--- client/src/component/Join.js
+++ client/src/component/Join.js
@@ -1,20 +1,24 @@
-import React from "react";
-//require("dotenv").config();
+import React from 'react'
+import logo from '../images/github-logo.png'
 
 const Join = () => {
-  console.log(process.env);
+  console.log(process.env)
   return (
-    <div>
-      <h1>Join</h1>
-      <button
-        onClick={() => {
-          window.location.href = `${process.env.REACT_APP_BACKEND}/login`;
-        }}
-      >
-        Github login
-      </button>
+    <div className="joinOuter">
+      <div className="joinContainer">
+        <h2>Welcome to WhatStack</h2>
+        <img src={logo} alt="Logo" style={{ width: 80, height: 80 }} />
+        <button
+          className="joinButton"
+          onClick={() => {
+            window.location.href = `${process.env.REACT_APP_BACKEND}/login`
+          }}
+        >
+          Join with GitHub
+        </button>
+      </div>
     </div>
-  );
-};
+  )
+}
 
-export default Join;
+export default Join
