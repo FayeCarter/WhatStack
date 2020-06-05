f800a01497291997b2011b558ec6c071fb4214ec
Phil Vigus <philipvigus@yahoo.co.uk>
2020-06-05 12:07:05 +0100

    Refactor: remove old server tests

--- __tests__/server.test.js
+++ __tests__/server.test.js
@@ -25,12 +25,27 @@ describe("Server", () => {
     done();
   });
 
-  test("should send a list of rooms when requested", async () => {
-    console.log("here");
+  test("should send a list of rooms when requested", (done) => {
     socket.emit("requestRoomList");
-    socket.on("roomList", ({ roomlist }) => {
-      console.log("received room list");
-      expect(roomlist).toBe(["C++", "Python"]);
+    socket.on("roomList", (message) => {
+      expect(message.roomList).toContain("How do I get a job at WhatStack?");
+      done();
+    });
+  });
+
+  test("should broadcast messages sent by users", (done) => {
+    socket.emit("join", {
+      room: "How do I use constants in Lua?",
+      name: "Phil",
+    });
+    socket.emit("message", {
+      name: "A random github user (test)",
+      message: "This is a message",
+      room: "How do I use constants in Lua?",
+    });
+    socket.on("message", (message) => {
+      expect(message.message).toBe("This is a message");
+      done();
     });
   });
 });
